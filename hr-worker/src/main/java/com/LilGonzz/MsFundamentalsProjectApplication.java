8b3d045e32ef3f6386d9dfa3d63443258f52023b
LilGonzz <lucasgonzzagas@gmail.com>
2022-11-09 10:03:49 -0300

    first commit

---
+++ hr-worker/src/main/java/com/LilGonzz/MsFundamentalsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.LilGonzz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsFundamentalsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsFundamentalsProjectApplication.class, args);
+	}
+
+}
---
+++ hr-worker/src/test/java/com/LilGonzz/MsFundamentalsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LilGonzz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsFundamentalsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
