3b3dd9e6b9823096c10d95988f18d7b791fb5a55
LilGonzz <lucasgonzzagas@gmail.com>
2022-11-14 14:06:07 -0300

    folders hr-payroll

---
+++ hr-payroll/src/main/java/com/LilGonzz/hrpayroll/model/Payment.java
@@ -0,0 +1,15 @@
+package com.LilGonzz.hrpayroll.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Payment implements Serializable {
+
+
+}
